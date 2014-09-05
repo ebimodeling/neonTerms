0abcffaa47f1e79a2766523e6b2692e55903ebae
Edmund Hart <edmund.m.hart@gmail.com>
2014-09-05 12:28:55 -0600

    Fixed issue with addBuff where if input and buffer length were the same it failed.

--- R/createTbl.R
+++ R/createTbl.R
@@ -25,19 +25,3 @@ createTbl <- function(tbl,db,namelist,types){
   dbSendQuery(conn = dbC, fullq)
   
 }
-
-#' Test table
-#' @description simple helper function to test whether or not a table exists
-#' @param tbl the table name you want to check
-#' @param db the database to find the table in
-#' @return TRUE if exists, FALSE if not
-#' @export
-
-testTbl <- function(tbl,db){
-  drv <- dbDriver("SQLite")
-  dbC <- dbConnect(drv, dbname=db)
-  q1 <- paste("PRAGMA table_info(",tbl,")",sep="")
-  tbltest <- dbGetQuery(conn = dbC, q1)
-  
-  return(!is.null(tbltest))
-}
--- R/genID.R
+++ R/genID.R
@@ -16,17 +16,3 @@ genFieldUID <- function(out,db){
   return(apply(fullIDs,1,paste,collapse="."))
   
 }
-
-#' add buffer
-#' @export
-
-addBuff <- function(buf, num){
-  num <- as.character(num)
-  if(buf-nchar(num) > 0){
-    tocat <- paste(rep("0",(buf-nchar(num))),collapse="")
-  } else {
-    stop("Your buffer character size is too small, make it BIG!")
-  }
-  return(paste(tocat,num,sep=""))
-  
-}
